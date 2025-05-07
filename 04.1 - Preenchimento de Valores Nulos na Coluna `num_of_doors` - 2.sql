8eb28ca39938f225d7ed1e43c7298cac0c9c4b44
Francisco Douglas <145362658+frcdouglas@users.noreply.github.com>
2025-05-06 23:17:26 -0300

    Update and rename 4.1 - Preenchimento de Valores Nulos na Coluna `num_of_doors` - 2.sql to 04.1 - Preenchimento de Valores Nulos na Coluna `num_of_doors` - 2.sql

--- 04.1 - Preenchimento de Valores Nulos na Coluna `num_of_doors` - 2.sql
+++ 04.1 - Preenchimento de Valores Nulos na Coluna `num_of_doors` - 2.sql
@@ -2,10 +2,10 @@
 -- possuem 4 portas, portanto:
 -- Checking with sales a manegement, all Dodge gas and all Mazda diesel sold had four doors, so: 
 UPDATE
-  [automobile_data_coursera1].[dbo].[automobile_data]
+Â  [automobile_data_coursera1].[dbo].[automobile_data]
 SET
-  num_of_doors = 'four'
+Â  num_of_doors = 'four'
 WHERE
-  make = 'mazda'
-  AND fuel_type = 'diesel'
-  AND body_style = 'sedan';
+Â  make = 'mazda'
+Â  AND fuel_type = 'diesel'
+Â  AND body_style = 'sedan';
