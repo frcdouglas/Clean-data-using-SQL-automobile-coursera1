e56b541937cbf5585cc6102567c9d10bdb2ddefd
Francisco Douglas <145362658+frcdouglas@users.noreply.github.com>
2025-05-06 23:17:45 -0300

    Update and rename 6 - Correção na Coluna `num_of_cylinders` - 2.sql to 06 - Correção na Coluna `num_of_cylinders` - 2.sql

---
+++ 06 - Correção na Coluna `num_of_cylinders` - 2.sql
@@ -0,0 +1,6 @@
+UPDATE
+  [automobile_data_coursera1].[dbo].[automobile_data]
+SET
+  num_of_cylinders = 'two'
+WHERE
+  num_of_cylinders = 'tow';
--- 6 - Correção na Coluna `num_of_cylinders` - 2.sql
+++
@@ -1,6 +0,0 @@
-UPDATE
-  [automobile_data_coursera1].[dbo].[automobile_data]
-SET
-  num_of_cylinders = 'two'
-WHERE
-  num_of_cylinders = 'tow';
