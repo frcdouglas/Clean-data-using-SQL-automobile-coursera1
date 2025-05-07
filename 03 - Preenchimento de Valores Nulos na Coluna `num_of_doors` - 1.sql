66c95b06c3173145cbf6a2ec21da54b2075293ca
Francisco Douglas <145362658+frcdouglas@users.noreply.github.com>
2025-05-06 23:17:04 -0300

    Update and rename 3 - Preenchimento de Valores Nulos na Coluna `num_of_doors` - 1.sql to 03 - Preenchimento de Valores Nulos na Coluna `num_of_doors` - 1.sql

--- 03 - Preenchimento de Valores Nulos na Coluna `num_of_doors` - 1.sql
+++ 03 - Preenchimento de Valores Nulos na Coluna `num_of_doors` - 1.sql
@@ -3,7 +3,7 @@ SELECT
 FROM 
 	[automobile_data_coursera1].[dbo].[automobile_data]
 WHERE 
-	-- No SQL Server valores NULL em EM BRANCO s„o tratados de forma diferente
+	-- No SQL Server valores NULL em EM BRANCO s√£o tratados de forma diferente
 	-- In SQL Server NULL and BLANK values are treated different 
 	num_of_doors IS NULL OR num_of_doors = ''; 
 	
